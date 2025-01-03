47da02a3cd65913d7b30d9e442321b2e231f6f2e
Skyland Woll <skylandwoll@gmail.com>
2025-01-02 19:15:17 -0500

    SW - fmt

--- tangled/src/main.js
+++ tangled/src/main.js
@@ -1,65 +1,65 @@
-import './style.css'
+import './style.css';
 
 const STATES = {
-  "AL": { "name": "Alabama", "sightings": 40 },
-  "AK": { "name": "Alaska", "sightings": 11 },
-  "AS": { "name": "American Samoa", "sightings": 0 },
-  "AZ": { "name": "Arizona", "sightings": 22 },
-  "AR": { "name": "Arkansas", "sightings": 3 },
-  "CA": { "name": "California", "sightings": 5 },
-  "CO": { "name": "Colorado", "sightings": 6 },
-  "CT": { "name": "Connecticut", "sightings": 7 },
-  "DE": { "name": "Delaware", "sightings": 8 },
-  "DC": { "name": "District of Columbia", "sightings": 19 },
-  "FL": { "name": "Florida", "sightings": 50 },
-  "FM": { "name": "Federated States of Micronesia", "sightings": 10 },
-  "GA": { "name": "Georgia", "sightings": 11 },
-  "GU": { "name": "Guam", "sightings": 12 },
-  "HI": { "name": "Hawaii", "sightings": 13 },
-  "ID": { "name": "Idaho", "sightings": 4 },
-  "IL": { "name": "Illinois", "sightings": 5 },
-  "IN": { "name": "Indiana", "sightings": 6 },
-  "IA": { "name": "Iowa", "sightings": 7 },
-  "KS": { "name": "Kansas", "sightings": 8 },
-  "KY": { "name": "Kentucky", "sightings": 9 },
-  "LA": { "name": "Louisiana", "sightings": 20 },
-  "ME": { "name": "Maine", "sightings": 10 },
-  "MD": { "name": "Maryland", "sightings": 11 },
-  "MH": { "name": "Marshall Islands", "sightings": 1 },
-  "MA": { "name": "Massachusetts", "sightings": 2 },
-  "MI": { "name": "Michigan", "sightings": 7 },
-  "MN": { "name": "Minnesota", "sightings": 16 },
-  "MS": { "name": "Mississippi", "sightings": 23 },
-  "MO": { "name": "Missouri", "sightings": 6 },
-  "MT": { "name": "Montana", "sightings": 1 },
-  "MP": { "name": "Northern Mariana Islands", "sightings": 1 },
-  "NE": { "name": "Nebraska", "sightings": 1 },
-  "NV": { "name": "Nevada", "sightings": 1 },
-  "NH": { "name": "New Hampshire", "sightings": 1 },
-  "NJ": { "name": "New Jersey", "sightings": 55 },
-  "NM": { "name": "New Mexico", "sightings": 1 },
-  "NY": { "name": "New York", "sightings": 1 },
-  "NC": { "name": "North Carolina", "sightings": 1 },
-  "ND": { "name": "North Dakota", "sightings": 1 },
-  "OH": { "name": "Ohio", "sightings": 1 },
-  "OK": { "name": "Oklahoma", "sightings": 1 },
-  "OR": { "name": "Oregon", "sightings": 1 },
-  "PW": { "name": "Palau", "sightings": 1 },
-  "PA": { "name": "Pennsylvania", "sightings": 1 },
-  "PR": { "name": "Puerto Rico", "sightings": 1 },
-  "RI": { "name": "Rhode Island", "sightings": 1 },
-  "SC": { "name": "South Carolina", "sightings": 1 },
-  "SD": { "name": "South Dakota", "sightings": 1 },
-  "TN": { "name": "Tennessee", "sightings": 1 },
-  "TX": { "name": "Texas", "sightings": 1 },
-  "UT": { "name": "Utah", "sightings": 1 },
-  "VI": { "name": "U.S. Virgin Islands", "sightings": 1 },
-  "VT": { "name": "Vermont", "sightings": 1 },
-  "VA": { "name": "Virginia", "sightings": 1 },
-  "WA": { "name": "Washington", "sightings": 1 },
-  "WV": { "name": "West Virginia", "sightings": 1 },
-  "WI": { "name": "Wisconsin", "sightings": 1 },
-  "WY": { "name": "Wyoming", "sightings": 1 }
+  AL: { name: 'Alabama', sightings: 40 },
+  AK: { name: 'Alaska', sightings: 11 },
+  AS: { name: 'American Samoa', sightings: 0 },
+  AZ: { name: 'Arizona', sightings: 22 },
+  AR: { name: 'Arkansas', sightings: 3 },
+  CA: { name: 'California', sightings: 5 },
+  CO: { name: 'Colorado', sightings: 6 },
+  CT: { name: 'Connecticut', sightings: 7 },
+  DE: { name: 'Delaware', sightings: 8 },
+  DC: { name: 'District of Columbia', sightings: 19 },
+  FL: { name: 'Florida', sightings: 50 },
+  FM: { name: 'Federated States of Micronesia', sightings: 10 },
+  GA: { name: 'Georgia', sightings: 11 },
+  GU: { name: 'Guam', sightings: 12 },
+  HI: { name: 'Hawaii', sightings: 13 },
+  ID: { name: 'Idaho', sightings: 4 },
+  IL: { name: 'Illinois', sightings: 5 },
+  IN: { name: 'Indiana', sightings: 6 },
+  IA: { name: 'Iowa', sightings: 7 },
+  KS: { name: 'Kansas', sightings: 8 },
+  KY: { name: 'Kentucky', sightings: 9 },
+  LA: { name: 'Louisiana', sightings: 20 },
+  ME: { name: 'Maine', sightings: 10 },
+  MD: { name: 'Maryland', sightings: 11 },
+  MH: { name: 'Marshall Islands', sightings: 1 },
+  MA: { name: 'Massachusetts', sightings: 2 },
+  MI: { name: 'Michigan', sightings: 7 },
+  MN: { name: 'Minnesota', sightings: 16 },
+  MS: { name: 'Mississippi', sightings: 23 },
+  MO: { name: 'Missouri', sightings: 6 },
+  MT: { name: 'Montana', sightings: 1 },
+  MP: { name: 'Northern Mariana Islands', sightings: 1 },
+  NE: { name: 'Nebraska', sightings: 1 },
+  NV: { name: 'Nevada', sightings: 1 },
+  NH: { name: 'New Hampshire', sightings: 1 },
+  NJ: { name: 'New Jersey', sightings: 55 },
+  NM: { name: 'New Mexico', sightings: 1 },
+  NY: { name: 'New York', sightings: 1 },
+  NC: { name: 'North Carolina', sightings: 1 },
+  ND: { name: 'North Dakota', sightings: 1 },
+  OH: { name: 'Ohio', sightings: 1 },
+  OK: { name: 'Oklahoma', sightings: 1 },
+  OR: { name: 'Oregon', sightings: 1 },
+  PW: { name: 'Palau', sightings: 1 },
+  PA: { name: 'Pennsylvania', sightings: 1 },
+  PR: { name: 'Puerto Rico', sightings: 1 },
+  RI: { name: 'Rhode Island', sightings: 1 },
+  SC: { name: 'South Carolina', sightings: 1 },
+  SD: { name: 'South Dakota', sightings: 1 },
+  TN: { name: 'Tennessee', sightings: 1 },
+  TX: { name: 'Texas', sightings: 1 },
+  UT: { name: 'Utah', sightings: 1 },
+  VI: { name: 'U.S. Virgin Islands', sightings: 1 },
+  VT: { name: 'Vermont', sightings: 1 },
+  VA: { name: 'Virginia', sightings: 1 },
+  WA: { name: 'Washington', sightings: 1 },
+  WV: { name: 'West Virginia', sightings: 1 },
+  WI: { name: 'Wisconsin', sightings: 1 },
+  WY: { name: 'Wyoming', sightings: 1 },
 };
 
 const appTarget = document.getElementById('app');
@@ -115,12 +115,13 @@ function updateMap() {
 
     // Handle click on the state.
     function handleStateClick() {
-
       // Get the LI for this state.
-      const stateButton = document.querySelector(`button[data-state="${state}"]`);
+      const stateButton = document.querySelector(
+        `button[data-state="${state}"]`
+      );
 
       // Remove the active class from all states.
-      document.querySelectorAll('.state-button').forEach(li => {
+      document.querySelectorAll('.state-button').forEach((li) => {
         li.classList.remove('active');
       });
 
@@ -159,12 +160,17 @@ function updateMap() {
   if (isChoropleth) {
     console.log('is chloro!');
     // Determine the max number of sightings.
-    const maxSightings = Math.max(...Object.values(STATES).map(state => state.sightings));
+    const maxSightings = Math.max(
+      ...Object.values(STATES).map((state) => state.sightings)
+    );
 
     // Set % var on each state LI.
     for (const state in STATES) {
       const stateLI = document.querySelector(`.${state}`);
-      stateLI.style.setProperty('--sightings', `${STATES[state].sightings / maxSightings * 100}%`);
+      stateLI.style.setProperty(
+        '--sightings',
+        `${(STATES[state].sightings / maxSightings) * 100}%`
+      );
     }
 
     choroplethCheckbox.checked = true;
