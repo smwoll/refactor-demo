baab7ee7197797499564f7f61591c019836b47a3
Skyland Woll <skylandwoll@gmail.com>
2025-01-03 12:05:09 -0500

    SW - update cleaned functional example

--- cleaned-functional/src/main.js
+++ cleaned-functional/src/main.js
@@ -7,7 +7,6 @@ import './style.css';
  * @returns {Promise<Object>} The states data.
  */
 async function getAndCacheStates() {
-
   // Check if states are in sessionStorage.
   const cached = sessionStorage.getItem('drone-app-messy-example-states');
 
@@ -16,38 +15,34 @@ async function getAndCacheStates() {
   }
 
   // For example purposes, we fetch the states data from a JSON file hosted in the GH repo.
-  const response = await fetch('https://raw.githubusercontent.com/smwoll/refactor-demo/refs/heads/main/state-data.json');
+  const response = await fetch(
+    'https://raw.githubusercontent.com/smwoll/refactor-demo/refs/heads/main/state-data.json'
+  );
   const states = await response.json();
 
   // Cache in sessionStorage.
-  sessionStorage.setItem('drone-app-messy-example-states', JSON.stringify(states));
+  sessionStorage.setItem(
+    'drone-app-messy-example-states',
+    JSON.stringify(states)
+  );
 
   return states;
 }
 
-
-function createMap() {
+async function createMap() {
   const map = document.getElementById('map');
 
   // Add states grid list.
   const statesList = document.createElement('ul');
   statesList.classList.add('states-list');
   map.appendChild(statesList);
-}
-
-async function updateMap() {
 
   const statesData = await getAndCacheStates();
 
-  const statesList = document.querySelector('.states-list');
-
-  const infoBox = document.getElementById('state-info-box');
-
-  // Clear out any states in list.
-  statesList.innerHTML = '';
-
-  // Clear out any state info.
-  infoBox.innerHTML = '';
+  // Determine the max number of sightings.
+  const maxSightings = Math.max(
+    ...Object.values(statesData).map((state) => state.sightings)
+  );
 
   // Add a state LI for each state.
   for (const state in statesData) {
@@ -56,6 +51,12 @@ async function updateMap() {
     stateLI.classList.add('state-item');
     stateLI.classList.add(state);
     statesList.appendChild(stateLI);
+
+    // Set the % var on each state LI.
+    stateLI.style.setProperty(
+      '--sightings',
+      `${(statesData[state].sightings / maxSightings) * 100}%`
+    );
 
     // Add button within each state LI.
     const stateButton = document.createElement('button');
@@ -115,50 +116,41 @@ async function updateMap() {
 
     stateLI.appendChild(stateButton);
   }
+}
 
-  const isChoropleth = map.classList.contains('choropleth-mode');
+function resetInfoBox() {
+  const infoBox = document.getElementById('state-info-box');
+  infoBox.innerHTML = '';
+}
 
+function handleChoroplethToggle(event) {
+
+  // Reset info box when changing modes.
+  resetInfoBox();
+
+  const app = document.getElementById('app');
+
+  const checked = event.target.checked;
+
+  app.dataset.isChoropleth = checked;
+
+  const rangeEl = document.querySelector('.chloropleth-range');
+
+  // Hide the range if not choropleth.
+  rangeEl.hidden = !checked;
+}
+
+
+function setupEventListeners() {
   const choroplethCheckbox = document.getElementById('choropleth-toggle');
-
-  if (isChoropleth) {
-    console.log('is chloro!');
-    // Determine the max number of sightings.
-    const maxSightings = Math.max(
-      ...Object.values(statesData).map((state) => state.sightings)
-    );
-
-    // Set % var on each state LI.
-    for (const state in statesData) {
-      const stateLI = document.querySelector(`li[data-state="${state}"]`);
-      stateLI.style.setProperty(
-        '--sightings',
-        `${(statesData[state].sightings / maxSightings) * 100}%`
-      );
-    }
-
-    choroplethCheckbox.checked = true;
-
-    const rangeEl = document.querySelector('.chloropleth-range');
-    rangeEl.hidden = false;
-  } else {
-    console.log('not chloro!');
-    choroplethCheckbox.checked = false;
-
-    const rangeEl = document.querySelector('.chloropleth-range');
-    rangeEl.hidden = true;
-  }
-
-  // Add event listener to toggle choropleth.
-  function handleChoroplethToggle() {
-    console.log('toggling choropleth', !isChoropleth);
-    map.classList.toggle('choropleth-mode');
-    updateMap();
-  }
-
-  // Add event listener to the checkbox.
   choroplethCheckbox.addEventListener('change', handleChoroplethToggle);
 }
 
-createMap();
+async function setupMapApplication() {
+  await createMap();
 
-updateMap();
+  setupEventListeners();
+  
+}
+
+setupMapApplication();
